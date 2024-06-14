5460b1e54a4902f030a9536b6030b77dc8584ddb
lwb <wb.lv@qq.com>
2024-06-14 10:25:44 +0800

    完善了ants_paths_follower下的cubic_spline.h和cubic_spline.cpp ToDo： 1.完善CubicSplineClass 2.更改成员变量的访问权限

--- src/bag_ants/ants_paths_follower/include/ants_paths_follower/cubic_spline.h
+++ src/bag_ants/ants_paths_follower/include/ants_paths_follower/cubic_spline.h
@@ -1,34 +1,36 @@
+/*
+三次样条插值曲线
+*/
 #ifndef CUBIC_SPLINE_PLANNER_H
 #define CUBIC_SPLINE_PLANNER_H
 
-#include <Eigen/Eigen>
-#include <ros/ros.h>
-#include <nav_msgs/Path.h>
 #include <nav_msgs/Odometry.h>
-#include <tf/transform_datatypes.h>
-#include <pcl_conversions/pcl_conversions.h>
+#include <nav_msgs/Path.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/kdtree/kdtree_flann.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-#include <pcl/filters/voxel_grid.h>
-#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl_conversions/pcl_conversions.h>
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
 #include <sensor_msgs/PointCloud2.h>
-#include <pcl/filters/passthrough.h>
-#include <pcl/features/normal_3d.h>
-#include <pcl/kdtree/kdtree.h>
-#include <pcl/kdtree/kdtree_flann.h>
-#include <pcl/sample_consensus/method_types.h>
-#include <pcl/sample_consensus/model_types.h>
-#include <pcl/segmentation/sac_segmentation.h>
-#include <pcl/segmentation/extract_clusters.h>
-#include <pcl/filters/extract_indices.h>
+#include <tf/transform_datatypes.h>
+
+#include <Eigen/Eigen>
 // #include <routing/Astar_searcher.h>
 // #include <custom_messages/mapmodel.h>
-#include "geometry_msgs/PoseArray.h"
+#include <geometry_msgs/PoseArray.h>
 // #include <custom_messages/planning_info.h>
-#include "common_private_msgs/vehicle_status.h"
 #include "common_private_msgs/planning_info.h"
+#include "common_private_msgs/vehicle_status.h"
 
 #define PI 3.1415926
 
@@ -44,149 +46,134 @@ std::vector<double> vec_diff(const std::vector<double> &input);
 */
 std::vector<double> cum_sum(const std::vector<double> &input);
 
-class Spline
-{
+// 定义结构体点
+struct Point {
+    double x = 0.0;
+    double y = 0.0;
+};
+/*三次样条曲线插值最终得到每个区间拟合的系数
+  d_i * (x-x_i)^3 + c_i * (x-x_i)^2 + b_i * (x-x_i) + a_i
+*/
+class Spline {
 public:
-    std::vector<double> x,y;               //x为弧长，y为对应的坐标
-    std::vector<double> h;				  //y的步长
-    std::vector<double> a,b,c,d;      //三次多项式的四个系数，注意a是最低次项，依次升高
-    int nx;                     //区间数，即一共几段要拟合的曲线
+    std::vector<double> x, y;      // x为横坐标，y为函数值坐标
+    std::vector<double> h;         // x的步长
+    std::vector<double> a, b, c, d;// 三次多项式的四个系数，注意a为最低次项，依次升高
+    int nx = 0;                    // 区间数，即一共有几段要拟合的曲线
+public:
+    // 构造函数
+    Spline() = default;
+    Spline(const std::vector<double> &x_, const std::vector<double> &y_);
 
-    //默认构造函数
-    Spline()
-    {
-    }
-    /*普通构造函数，最终得到每个区间拟合的系数
-    d_i * (x-x_i)^3 + c_i * (x-x_i)^2 + b_i * (x-x_i) + a_i
-    */
-    Spline(std::vector<double> x_,std::vector<double> y_):x(x_),y(y_),nx(x_.size()),h(vec_diff(x_)),a(y_)
-    {
-        //构造以系数c为未知量的矩阵方程并求解，公式易知，直接可输入此矩阵方程的系数矩阵A和解向量B
-        Eigen::MatrixXd A;
-        Eigen::VectorXd B,c_temp;
-        A=calc_A();
-        B=calc_B();
-        c_temp=A.colPivHouseholderQr().solve(B);
-        // 存储顺序默认使用列存储，可以使用data()函数获得存储数据的首地址
-        double *c_pointer = c_temp.data();
-        /* Map类用于通过C++中普通的连续指针或者数组 （raw C/C++arrays）来构造Eigen里的Matrix类，
-            这就好比Eigen里的Matrix类的数据和raw C++array共享了一片地址，也就是引用。
-            比如有个API只接受普通的C++数组，但又要对普通数组进行线性代数操作，那么用它构造为Map类，
-            直接操作Map就等于操作了原始普通数组，省时省力。
-            STL中不同容器之间是不能直接赋值的，assign（）可以实现不同容器但相容的类型赋值*/
-        c.assign(c_pointer, c_pointer + c_temp.rows());
-        //套公式求出系数b和d
-        for(int i=0; i<nx-1; i++)
-        {
-            d.push_back((c[i+1] - c[i]) / (3.0 * h[i]));
-            b.push_back((a[i+1] - a[i]) / h[i] - h[i] * (c[i+1] + 2*c[i])/3.0);
-        }
-    }
-
-    //求t位置的0阶导数
+    // 求t位置的0阶导数
     double calc(double t);
-    //求t位置的1阶导数
+    // 求t位置的1阶导数
     double calc_d(double t);
-    //求t位置的2阶导数
+    // 求t位置的2阶导数
     double calc_dd(double t);
 
 private:
+    // 生成系数矩阵
     Eigen::MatrixXd calc_A();
-
+    //生成解向量
     Eigen::VectorXd calc_B();
-
-    /*递归二分法找根*/
+    //递归二分法找根
     int bisect(double t, int start, int end);
 };
-
-class Spline2D
-{
+/*
+ 二维平面三次样条曲线
+https://blog.csdn.net/u013468614/article/details/108416552
+ */
+class Spline2D {
 public:
-    Spline sx;
-    Spline sy;
-    std::vector<double> s;                  //弧长
-    //构造函数
-    Spline2D(){};
-    Spline2D(std::vector<double> x,std::vector<double> y)
-    {
-        s=calc_s(x,y);
-        sx=Spline(s,x);
-        sy=Spline(s,y);
-    }
+    Spline sx;            // x的样条曲线
+    Spline sy;            // y的样条曲线
+    std::vector<double> s;// 弧长，样条曲线的参数
 
-    //位置计算函数
-    std::array<double,2> calc_postion(double s_t);
-    //曲率计算函数
+public:
+    // 构造函数
+    Spline2D() = default;
+    Spline2D(const std::vector<double> &x, const std::vector<double> &y);
+
+    // 位置计算函数
+    Point calc_position(double s_t);
+    // 曲率计算函数
     double calc_curvature(double s_t);
-    //航向角计算函数
+    // 航向角计算函数
     double calc_yaw(double s_t);
-
-    //弧长计算函数，输出数列为从起点到当前位置的弧长
-    std::vector<double> calc_s(std::vector<double> x, std::vector<double> y);
+    // 弧长计算函数，输出数列为从起点到当前位置的弧长
+    std::vector<double> calc_s(const std::vector<double> &x, const std::vector<double> &y);
+    // 返回总弧长
+    double length();
 };
 
-struct SplineCarState
-{
+struct SplineCarState {
     double x;
     double y;
     double yaw;
     double speed;
     double angular_speed;
     // SplineCarState(){};
-    // SplineCarState(double x_, double y_, double yaw_, double speed_, double angular_speed_):
+    // SplineCarState(double x_, double y_, double yaw_, double speed_, double
+    // angular_speed_):
     //     x(x_), y(y_), yaw(yaw_), speed(speed_), angular_speed(angular_speed_)
     // {}
 };
 
-class CSRefPath
-{
+class CSRefPath {
 public:
-    CSRefPath(){};
-    ~CSRefPath(){};
-    void CSclear();
-    void load(Spline2D &spline, double overall_length);
-    void joint(CSRefPath &path);
-    void copypath(CSRefPath &path, int index);
-    
-    CSRefPath& operator=(CSRefPath& input);
+    std::vector<double> r_x;        // x坐标
+    std::vector<double> r_y;        // y坐标
+    std::vector<double> r_yaw;      // 航向角
+    std::vector<double> r_curvature;// 曲率
+    std::vector<double> r_s;        // 弧长
 
-    std::vector<double> r_x;
-    std::vector<double> r_y;
-    std::vector<double> r_yaw;
-    std::vector<double> r_curvature;
-    std::vector<double> r_s;
+public:
+    CSRefPath() = default;
+    ~CSRefPath() = default;
+    void CSclear();
+    void load(Spline2D &spline, double overall_length);// 载入路径
+    void joint(const CSRefPath &path);                 // 添加路径
+    void copypath(const CSRefPath &path, int index);   // 从索引开始添加路径，索引从0开始
+
+    CSRefPath &operator=(const CSRefPath &input);
 };
 
 // plannercore
-class CubicSplineClass
-{
+class CubicSplineClass {
 public:
     CubicSplineClass(ros::Publisher trajectory_pub);
-    void FrontCallback(const nav_msgs::Odometry::ConstPtr& front_msg);
-    void LeaderCallback(const nav_msgs::Odometry::ConstPtr& leader_msg);
-    void SelfCallback(const nav_msgs::Odometry::ConstPtr& self_msg);
+    void FrontCallback(const nav_msgs::Odometry::ConstPtr &front_msg);
+    void LeaderCallback(const nav_msgs::Odometry::ConstPtr &leader_msg);
+    void SelfCallback(const nav_msgs::Odometry::ConstPtr &self_msg);
     // void ObsCallback(const custom_messages::mapmodel::ConstPtr& obs_msg);
-    // bool CollisionCheck(const CSRefPath &r_path, const SplineCarState &carstate, const AstarPathFinder* tmpmap, int mode); // mode=0,static map; mode=1, static map and dynamic obstacle
+    // bool CollisionCheck(const CSRefPath &r_path, const SplineCarState
+    // &carstate, const AstarPathFinder* tmpmap, int mode); // mode=0,static map;
+    // mode=1, static map and dynamic obstacle
     void planning();
-    void PlatoonCallback1(const nav_msgs::Odometry::ConstPtr& p_msg);
-    void PlatoonCallback2(const nav_msgs::Odometry::ConstPtr& p_msg);
-    void PlatoonCallback3(const nav_msgs::Odometry::ConstPtr& p_msg);
-    void PlatoonCallback4(const nav_msgs::Odometry::ConstPtr& p_msg);
-    void PlatoonCallback5(const nav_msgs::Odometry::ConstPtr& p_msg);
+    void PlatoonCallback1(const nav_msgs::Odometry::ConstPtr &p_msg);
+    void PlatoonCallback2(const nav_msgs::Odometry::ConstPtr &p_msg);
+    void PlatoonCallback3(const nav_msgs::Odometry::ConstPtr &p_msg);
+    void PlatoonCallback4(const nav_msgs::Odometry::ConstPtr &p_msg);
+    void PlatoonCallback5(const nav_msgs::Odometry::ConstPtr &p_msg);
 
-    bool GetVehicleInfo(int mode, int index, geometry_msgs::PoseStamped& out); // mode=0:front, mode=1:leader
-    bool GetVehiclemyInfo(int index, SplineCarState& out);
-    void ReconfigurationCallback(const geometry_msgs::Point::ConstPtr& reconstruction_msg);
+    bool GetVehicleInfo(
+            int mode, int index,
+            geometry_msgs::PoseStamped &out);// mode=0:front, mode=1:leader
+    bool GetVehiclemyInfo(int index, SplineCarState &out);
+    void ReconfigurationCallback(
+            const geometry_msgs::Point::ConstPtr &reconstruction_msg);
     // void UpdateKeeper(custom_messages::vehicle_status last_back);
     void UpdateKeeper(common_private_msgs::vehicle_status last_back);
-    int findMatchpoint(double x, double y, CSRefPath& input_trajectory);
+    int findMatchpoint(double x, double y, CSRefPath &input_trajectory);
     bool add_check(SplineCarState &last_front);
     bool add_lateral_check(SplineCarState &last_front);
     bool add_yaw_check(SplineCarState &last_front);
-    
+
     // formation change
     void update_formation(double formation_length_unit);
 
+public:
     std::vector<SplineCarState> keeper;
     ros::Publisher _trajectory_pub;
     geometry_msgs::PoseStamped front;
--- src/bag_ants/ants_paths_follower/src/cubic_spline.cpp
+++ src/bag_ants/ants_paths_follower/src/cubic_spline.cpp
@@ -1,5 +1,7 @@
 #include "cubic_spline.h"
 
+#include <utility>
+
 CubicSplineClass::CubicSplineClass(ros::Publisher trajectory_pub) {
     _trajectory_pub = trajectory_pub;
     keeper.resize(3);
@@ -7,12 +9,12 @@ CubicSplineClass::CubicSplineClass(ros::Publisher trajectory_pub) {
     formation_triangle.resize(5);
 }
 
-CSRefPath& CSRefPath::operator=(CSRefPath& input) {
-    std::vector<double> ().swap(r_curvature);
-    std::vector<double> ().swap(r_s);
-    std::vector<double> ().swap(r_x);
-    std::vector<double> ().swap(r_y);
-    std::vector<double> ().swap(r_yaw);
+CSRefPath &CSRefPath::operator=(const CSRefPath &input) {
+    std::vector<double>().swap(r_curvature);// 清空内存
+    std::vector<double>().swap(r_s);
+    std::vector<double>().swap(r_x);
+    std::vector<double>().swap(r_y);
+    std::vector<double>().swap(r_yaw);
     this->r_curvature = input.r_curvature;
     this->r_s = input.r_s;
     this->r_x = input.r_x;
@@ -21,36 +23,40 @@ CSRefPath& CSRefPath::operator=(CSRefPath& input) {
     return *this;
 }
 
-void CSRefPath::joint(CSRefPath &path){
-    double original_length = r_s.back();
+void CSRefPath::joint(const CSRefPath &path) {
+    this->copypath(path, 0);
     std::cout << "[cs_planner]: pre_joint_s = " << r_s.back() << std::endl;
     std::cout << "[cs_planner]: new_seg_s = " << path.r_s.back() << std::endl;
-    for(int i = 1; i < path.r_curvature.size(); ++i){
-        r_curvature.emplace_back(path.r_curvature[i]);
-        r_x.emplace_back(path.r_x[i]);
-        r_y.emplace_back(path.r_y[i]);
-        r_yaw.emplace_back(path.r_yaw[i]);
-        r_s.emplace_back(path.r_s[i] + original_length);
-    }
     std::cout << "[cs_planner]: post_joint_s = " << r_s.back() << std::endl;
 }
 
-void CSRefPath::copypath(CSRefPath &path, int index){
+void CSRefPath::copypath(const CSRefPath &path, int index) {
+    double original_length = r_s.back();
     double initial_length = path.r_s[index];
-    for(int i = index; i < path.r_curvature.size(); ++i){
-        r_curvature.emplace_back(path.r_curvature[i]);
-        r_x.emplace_back(path.r_x[i]);
-        r_y.emplace_back(path.r_y[i]);
-        r_yaw.emplace_back(path.r_yaw[i]);
-        r_s.emplace_back(path.r_s[i] - initial_length);
-        std::cout << "[cs_planner]: tmp_x = " << path.r_x[i] << ",tmp_y = " << path.r_y[i] << std::endl;
-        std::cout << "[cs_planner]: tmp_s = " << path.r_s[i] << "[" << i << "]" << std::endl;
-    }
+    int insert_size = static_cast<int>(path.r_s.size() - 1 - index);// 插入数据的数量
+    int capacity = static_cast<int>(r_s.size()) + insert_size;      // 应该分配的容量，第一个数据为当前位置，所以减1
+
+    r_curvature.reserve(capacity);
+    r_x.reserve(capacity);
+    r_y.reserve(capacity);
+    r_yaw.reserve(capacity);
+    r_s.reserve(capacity);
+
+    r_curvature.insert(r_curvature.end(), path.r_curvature.begin() + index + 1, path.r_curvature.end());// 将新数据插入到原数据后面
+    r_x.insert(r_x.end(), path.r_x.begin() + index + 1, path.r_x.end());
+    r_y.insert(r_y.end(), path.r_y.begin() + index + 1, path.r_y.end());
+    r_yaw.insert(r_yaw.end(), path.r_yaw.begin() + index + 1, path.r_yaw.end());
+    std::vector<double> diff_r_s;
+    diff_r_s.reserve(insert_size);
+    std::transform(path.r_s.end() - insert_size, path.r_s.end(), diff_r_s.begin(), [initial_length](int n) { return n - initial_length; });// 弧长是根据做差算出来的，所以提取时要减去第一个弧长
+    r_s.insert(r_s.end(), diff_r_s.begin(), diff_r_s.end());
+    auto new_data_ptr = r_s.end() - insert_size;
+    std::transform(new_data_ptr, r_s.end(), new_data_ptr, [original_length](int n) { return n + original_length; });// 弧长是根据做差算出来的，只是一组的弧长数据，所以需要加上原数据弧长累计值
     std::cout << "[cs_planner]: match_point_s = " << initial_length << "[" << index << "]" << std::endl;
     std::cout << "[cs_planner]: last_total_length = " << path.r_s.back() << ",now_total_length = " << r_s.back() << std::endl;
 }
 
-void CSRefPath::CSclear(){
+void CSRefPath::CSclear() {
     r_curvature.clear();
     r_s.clear();
     r_x.clear();
@@ -58,171 +64,181 @@ void CSRefPath::CSclear(){
     r_yaw.clear();
 }
 
-void CSRefPath::load(Spline2D &spline, double overall_length){
-    double refspline_length = spline.s.back();
-    double step = overall_length / 8;
-    for(double i = 0.0; i < refspline_length; i += step){
-        std::array<double, 2> point_ = spline.calc_postion(i);
-        r_x.emplace_back(point_[0]);
-        r_y.emplace_back(point_[1]);
-        r_yaw.emplace_back(spline.calc_yaw(i));
-        r_curvature.emplace_back(spline.calc_curvature(i));
-        r_s.emplace_back(i);
-    }
-    std::array<double,2> point_=spline.calc_postion(refspline_length);
-    r_x.emplace_back(point_[0]);
-    r_y.emplace_back(point_[1]);
-    r_yaw.emplace_back(spline.calc_yaw(refspline_length));
-    r_curvature.emplace_back(spline.calc_curvature(refspline_length));
+void CSRefPath::load(Spline2D &spline, double overall_length) {
+    double refspline_length = spline.length();                              // 获得总弧长
+    double step = overall_length / 8;                                       // 不懂
+    int num_elements = static_cast<int>(std::ceil(refspline_length / step));// 计算需要多少步长
+    r_s.reserve(num_elements + 1);
+    std::generate(r_s.begin(), r_s.begin() + num_elements, [n = 0.0, step]() mutable {
+        double current = n;
+        n += step;
+        return current;
+    });
     r_s.emplace_back(refspline_length);
+    r_x.reserve(r_s.size());
+    r_y.reserve(r_s.size());
+    r_yaw.reserve(r_s.size());
+    r_curvature.reserve(r_s.size());
+    for (const auto i: r_s) {
+        Point point_ = spline.calc_position(i);// 计算弧长为i时的坐标
+        r_x.emplace_back(point_.x);
+        r_y.emplace_back(point_.y);
+        r_yaw.emplace_back(spline.calc_yaw(i));            // 计算弧长为i时的航向角
+        r_curvature.emplace_back(spline.calc_curvature(i));// 计算弧长为i时的曲率
+    }
     std::cout << "[cs_planner]: CSRefPath_size=" << r_curvature.size() << std::endl;
 }
 
-std::vector<double> vec_diff(const std::vector<double> &input)
-{
+std::vector<double> vec_diff(const std::vector<double> &input) {
     std::vector<double> output;
-    for(unsigned int i=01;i<input.size();i++)
-    {
-        output.emplace_back(input[i]-input[i-1]);
-    }
+    output.reserve(input.size());// output容器要有足够的容量，否则报越界错误
+    std::adjacent_difference(input.begin(), input.end(), output.begin());
+    output.erase(output.begin());//第一个元素是原始数据
     return output;
 }
 
-std::vector<double> cum_sum(const std::vector<double> &input)
-{
+std::vector<double> cum_sum(const std::vector<double> &input) {
     std::vector<double> output;
-    double temp = 0;
-    for(unsigned int i=0; i<input.size(); i++)
-    {
-        temp += input[i];
-        output.emplace_back(temp);
-    }
+    output.reserve(input.size());
+    std::partial_sum(input.begin(), input.end(), output.begin());
     return output;
 }
 
-double Spline::calc(double t)
-{
-    if(t<x.front() || t>x.back())
-    {
-        throw std::invalid_argument( "received value out of the pre-defined range" );
+double Spline::calc(double t) {
+    if (t < x.front() || t > x.back()) {
+        throw std::invalid_argument("received value out of the pre-defined range");
     }
     int seg_id = bisect(t, 0, nx);
     double dx = t - x[seg_id];
     return a[seg_id] + b[seg_id] * dx + c[seg_id] * dx * dx + d[seg_id] * dx * dx * dx;
 };
 
-double Spline::calc_d(double t)
-{
-    if(t<x.front() || t>x.back())
-    {
-        throw std::invalid_argument( "received value out of the pre-defined range" );
+double Spline::calc_d(double t) {
+    if (t < x.front() || t > x.back()) {
+        throw std::invalid_argument("received value out of the pre-defined range");
     }
-    int seg_id = bisect(t, 0, nx-1);
+    int seg_id = bisect(t, 0, nx - 1);
     double dx = t - x[seg_id];
-    return b[seg_id]  + 2 * c[seg_id] * dx + 3 * d[seg_id] * dx * dx;
+    return b[seg_id] + 2 * c[seg_id] * dx + 3 * d[seg_id] * dx * dx;
 }
 
-double Spline::calc_dd(double t)
-{
-    if(t<x.front() || t>x.back())
-    {
-        throw std::invalid_argument( "received value out of the pre-defined range" );
+double Spline::calc_dd(double t) {
+    if (t < x.front() || t > x.back()) {
+        throw std::invalid_argument("received value out of the pre-defined range");
     }
     int seg_id = bisect(t, 0, nx);
     double dx = t - x[seg_id];
     return 2 * c[seg_id] + 6 * d[seg_id] * dx;
 }
 
-Eigen::MatrixXd Spline::calc_A()
-{
-    Eigen::MatrixXd A=Eigen::MatrixXd::Zero(nx,nx);
+Eigen::MatrixXd Spline::calc_A() {
+    Eigen::MatrixXd A = Eigen::MatrixXd::Zero(nx + 1, nx + 1);
     /*自由边界，即第一段第一个点和最后一点最后一个点的二阶导数为0
     */
-        A(0, 0) = 1;
-        for(int i=0; i<nx-1; i++)
-        {
-            if (i != nx-2)
-            {
-                A(i+1, i+1) = 2 * (h[i] + h[i+1]);
-            }
-            A(i+1, i) = h[i];
-            A(i, i+1) = h[i];
-        }
-        A(0, 1) = 0.0;
-        A(nx-1, nx-2) = 0.0;
-        A(nx-1, nx-1) = 1.0;
-        return A;
+    for (int i = 1; i < A.rows() - 1; ++i) {
+        A(i, i - 1) = h[i - 1];
+        A(i, i + 1) = h[i];
+        A(i, i) = 2 * (A(i, i - 1) + A(i, i + 1));
+    }
+    A(0, 0) = 2;
+    A(nx - 1, nx - 1) = 2;
+    return A;
     /*受约束边界，即第一段第一个点和最后一点最后一个点的一阶导数为给定值
     
     */
 }
 
-Eigen::VectorXd Spline::calc_B()
-{
+Eigen::VectorXd Spline::calc_B() {
     Eigen::VectorXd B = Eigen::VectorXd::Zero(nx);
-    for(int i=0; i<nx-2; i++)
-    {
-        B(i+1) = 3.0*(a[i+2]-a[i+1])/h[i+1] - 3.0*(a[i+1]-a[i])/h[i];
-    }
+    for (int i = 1; i < B.size() - 1; i++)
+        B(i) = (y[i + 1] - y[i]) / h[i] - (y[i] - y[i - 1]) / h[i - 1];
     return B;
 }
 
-int Spline::bisect(double t, int start, int end)
-{
-    int mid = (start+end)/2;
-    if (t==x[mid] || end-start<=1)
-    {
+int Spline::bisect(double t, int start, int end) {
+    int mid = (start + end) / 2;
+    if (t == x[mid] || end - start <= 1) {
         return mid;
-    }
-    else if (t>x[mid])
-    {
+    } else if (t > x[mid]) {
         return bisect(t, mid, end);
-    }
-    else
-    {
+    } else {
         return bisect(t, start, mid);
     }
 }
+Spline::Spline(const std::vector<double> &x_, const std::vector<double> &y_) : x(x_), y(y_), nx(x_.size() - 1), h(vec_diff(x_)) {
+    //推导可见https://zhuanlan.zhihu.com/p/672601034
+    // 构造以系数c为未知量的矩阵方程并求解，公式易知，直接可输入此矩阵方程的系数矩阵A和解向量B
+    Eigen::MatrixXd A;
+    Eigen::VectorXd B, m_temp;
+    A = calc_A();
+    B = calc_B();
+    m_temp = A.colPivHouseholderQr().solve(6 * B);
+    // 存储顺序默认使用列存储，可以使用data()函数获得存储数据的首地址
+    /* Map类用于通过C++中普通的连续指针或者数组 （raw
+  C/C++arrays）来构造Eigen里的Matrix类，
+   这就好比Eigen里的Matrix类的数据和raw
+  C++array共享了一片地址，也就是引用。
+   比如有个API只接受普通的C++数组，但又要对普通数组进行线性代数操作，那么用它构造为Map类，
+   直接操作Map就等于操作了原始普通数组，省时省力。
+   STL中不同容器之间是不能直接赋值的，assign（）可以实现不同容器但相容的类型赋值*/
+    std::vector<double> m(m_temp.data(), m_temp.data() + m_temp.size());
+    // 套公式求出系数b和d
+    a = y;
+    a.pop_back();//a为n-1个
+    c = m;
+    c.pop_back();//c为n-1个
+    b.resize(nx);
+    d.resize(nx);
+    for (int i = 0; i < nx; i++) {
+        d.emplace_back((m[i + 1] - m[i]) / (6 * h[i]));
+        b.emplace_back((y[i + 1] - y[i]) / h[i] - h[i] * m[i] / 2 - (m[i + 1] - m[i]) * h[i] / 6);
+    }
+}
 
-std::array<double,2> Spline2D::calc_postion(double s_t)
-{
-    double x = sx.calc(s_t);
-    double y = sy.calc(s_t);
-    return {{x, y}};
+
+Point Spline2D::calc_position(double s_t) {
+    Point point;
+    point.x = sx.calc(s_t);
+    point.y = sy.calc(s_t);
+    return point;
 };
 
-double Spline2D::calc_curvature(double s_t)
-{
+double Spline2D::calc_curvature(double s_t) {
     double dx = sx.calc_d(s_t);
     double ddx = sx.calc_dd(s_t);
     double dy = sy.calc_d(s_t);
     double ddy = sy.calc_dd(s_t);
 
-    // cy
+    // 曲率计算公式
     return (ddy * dx - ddx * dy) / pow((dx * dx + dy * dy), 1.5);
 };
 
-double Spline2D::calc_yaw(double s_t)
-{
+double Spline2D::calc_yaw(double s_t) {
     double dx = sx.calc_d(s_t);
     double dy = sy.calc_d(s_t);
     return std::atan2(dy, dx);
 };
 
-std::vector<double> Spline2D::calc_s(std::vector<double> x, std::vector<double> y)
-{
+std::vector<double> Spline2D::calc_s(const std::vector<double> &x, const std::vector<double> &y) {
     std::vector<double> ds;
-    std::vector<double> out_s{0};
+    ds.reserve(x.size());
     std::vector<double> dx = vec_diff(x);
     std::vector<double> dy = vec_diff(y);
-    for(unsigned int i=0; i<dx.size(); i++)
-    {
-        ds.emplace_back(std::sqrt(dx[i]*dx[i] + dy[i]*dy[i]));
-    }
-    std::vector<double> cum_ds = cum_sum(ds);
-    out_s.insert(out_s.end(), cum_ds.begin(), cum_ds.end());
-    return out_s;
+    std::transform(dx.begin(), dx.end(), dy.begin(), ds.begin(),
+                   [](double x, double y) { return std::sqrt(x * x + y * y); });
+    ds.emplace(ds.begin(), 0);// ds的数量需要与x一致
+    return cum_sum(ds);
 }
+Spline2D::Spline2D(const std::vector<double> &x, const std::vector<double> &y) {
+    if (x.size() != y.size()) {
+        std::cerr << "x与y的数量不相等!" << std::endl;
+    }
+    s = calc_s(x, y);
+    sx = Spline(s, x);
+    sy = Spline(s, y);
+}
+double Spline2D::length() { return s.back(); }
+
 
 void CubicSplineClass::UpdateKeeper(common_private_msgs::vehicle_status last_back) {
     keeper[0].x = mystate.x;
@@ -233,17 +249,15 @@ void CubicSplineClass::UpdateKeeper(common_private_msgs::vehicle_status last_bac
     keeper[1].yaw = last_back.yaw;
 }
 
-int CubicSplineClass::findMatchpoint(double x, double y, CSRefPath& input_trajectory){
+int CubicSplineClass::findMatchpoint(double x, double y, CSRefPath &input_trajectory) {
     int num = input_trajectory.r_curvature.size();
     double dis_min = std::numeric_limits<double>::max();
     int index = 0;
-    for(int i=0; i < num; i++)
-    {
+    for (int i = 0; i < num; i++) {
         double temp_dis = std::pow(input_trajectory.r_x[i] - x, 2) + std::pow(input_trajectory.r_y[i] - y, 2);
         //ROS_ERROR("%s,%f.%s,%f","r_x",r_x[i],"r_y",r_y[i]);
         //ROS_ERROR("%s,%f","distance",temp_dis);
-        if(temp_dis < dis_min)
-        {
+        if (temp_dis < dis_min) {
             dis_min = temp_dis;
             index = i;
         }
